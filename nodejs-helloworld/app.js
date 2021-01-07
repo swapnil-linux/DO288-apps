96895b6f478893c9044c00a14a9932cffb5a87a3
Swapnil Jain <swapnil@linux.com>
2021-01-07 13:55:53 +1100

    v2

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,10 +2,9 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World Version2!\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app V2 listening on port 8080!');
 });
-
