4ed27e44d8df70998dd0ffbf6ef616b68601f530
Swapnil Jain <swapnil@linux.com>
2021-04-30 16:25:00 +1000

    v4.0

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World Version3.7!</h1>\n');
+  res.send('<h1>Hello World Version4.0!</h1>\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app V3.5 listening on port 8080!');
+  console.log('Example app V4.0 listening on port 8080!');
 });
